e61c4440ae4723b6f68ec781a3c3a7dc108b7fc1
Mithila Arman <42859255+mithilaarman@users.noreply.github.com>
2022-12-29 18:49:26 +0600

    Add files via upload

---
+++ Slick  Slider/js/script.js
@@ -0,0 +1,9 @@
+$('.slick_slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed:500,
+    autoplaySpeed: 2000,
+    prevArrow:'<i class="fa-solid fa-angle-left left_arrow"></i>',
+    nextArrow:'<i class="fa-sharp fa-solid fa-angle-right right_arrow"></i>'
+  });
